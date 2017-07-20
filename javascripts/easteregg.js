f97b52f851e9f170e35377111dfd93242a65cbfe
bkupie <bkupie2@uic.edu>
2017-07-20 11:33:24 -0500

    :)

---
+++ javascripts/easteregg.js
@@ -0,0 +1,12 @@
+function changeImage()
+{
+var mainpicture = document.getElementById("main-picture");
+mainpicture.src = "../images/dog.gif";
+setTimeout(changeImageBack,5000);
+}
+
+function changeImageBack()
+{
+  var mainpicture = document.getElementById("main-picture");
+  mainpicture.src = "../images/me-circle.png";
+}
