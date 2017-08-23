abea2b656827ee181fb392e3e07b7c8f5744df13
bkupie <bkupie2@uic.edu>
2017-08-22 20:11:25 -0500

    Updated the on-image-click easter egg to use jquery properly

--- javascripts/easteregg.js
+++ javascripts/easteregg.js
@@ -1,7 +1,18 @@
+$(function () {
+    //set up on click functionality
+    $("#main-picture").click(function(){
+        changeImage();
+      });
+});
+
 function changeImage()
 {
   var mainpicture = document.getElementById("main-picture");
+  // specify the new image source
   mainpicture.src = "../images/dog.gif";
+  // disable the ability to click the image again
+  mainpicture.style.pointerEvents = 'none';
+  // timeout for ~5 seconds for the gif to play out 
   setTimeout(changeImageBack,5000);
 }
 
@@ -9,4 +20,6 @@ function changeImageBack()
 {
   var mainpicture = document.getElementById("main-picture");
   mainpicture.src = "../images/me-circle.png";
+  // re-enable the ability to click the image
+  mainpicture.style.pointerEvents = 'auto';
 }
